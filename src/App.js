a8b6fb29bdbe64a0d09435ce964d0e09a405254a
anbut02 <49370690+anbut02@users.noreply.github.com>
2019-04-08 23:26:44 +0530

    test

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ class App extends Component {
 
   render(){
     return(
-      <BrowserRouter>
       
       
-        <Route path="/opportunity/:opportunity_id" exact  name="Home" component={Home} />
-        <Route path="/Edit/:edit_id" exact name="Edit" component={Edit} />
-        <Route path="/" exact  name="notfound" component={NotFounds} />
-     
-    </BrowserRouter>
+      <div>
+        <Route path="/opportunity/:opportunity_id" exact  name="Home" render={ (routerProps) => < Home routerProps={routerProps} />}  />
+        <Route path="/Edit/:edit_id" exact name="Edit"render={ (routerProps) => < Edit routerProps={routerProps} />}  />
+        <Route path="/" exact  name="notfound" render={ (routerProps) => < NotFounds routerProps={routerProps} />}  />
+		</div>
     )
   }
 }
