d12604dfdcabbae46f1fd4dd52ddbe39f18c2653
anbut02 <49370690+anbut02@users.noreply.github.com>
2019-04-08 22:47:27 +0530

    test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch,Redirect } from 'react-router-dom';
 import Home from './Home';
-//import NotFounds from './components/NotFound'
-//import Edit from './components/Edit'
+import NotFounds from './components/NotFound'
+import Edit from './components/Edit'
 
 class App extends Component {
 
   render(){
     return(
-      <Home />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      
+      
+        <Route path={"/opportunity"} exact  name="Home" render={ (routerProps) => < Home routerProps={routerProps}/>} />
+        <Route path="/Edit/:edit_id" exact name="Edit" render={ (routerProps) => < Edit routerProps={routerProps}/>}/>
+        <Route path="/" exact  name="notfound" render={ (routerProps) => < NotFounds routerProps={routerProps}/>}  />
+     
+    </BrowserRouter>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import {BrowserRouter  } from 'react-router-dom';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
