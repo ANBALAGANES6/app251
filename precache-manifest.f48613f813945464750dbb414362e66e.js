fb450f88e62023eea1987f5ded7cf256f431345f
anbut02 <49370690+anbut02@users.noreply.github.com>
2019-04-09 00:09:54 +0530

    Updates

---
+++ precache-manifest.f48613f813945464750dbb414362e66e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ed551345daaab946b22",
+    "url": "http://ANBALAGANES6.github.io/app251/static/js/runtime~main.f32e76d6.js"
+  },
+  {
+    "revision": "14047b063d8f39059f3a",
+    "url": "http://ANBALAGANES6.github.io/app251/static/js/main.0ee7e401.chunk.js"
+  },
+  {
+    "revision": "2ed23edf952f1dfc6236",
+    "url": "http://ANBALAGANES6.github.io/app251/static/js/2.7041cd23.chunk.js"
+  },
+  {
+    "revision": "14047b063d8f39059f3a",
+    "url": "http://ANBALAGANES6.github.io/app251/static/css/main.006c298f.chunk.css"
+  },
+  {
+    "revision": "2ed23edf952f1dfc6236",
+    "url": "http://ANBALAGANES6.github.io/app251/static/css/2.2a36cb3f.chunk.css"
+  },
+  {
+    "revision": "3ef62b119f3647245d2663425622d69a",
+    "url": "http://ANBALAGANES6.github.io/app251/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/app251/precache-manifest.8d7a86ff18eaa2ae6b113cfbc5cba59f.js"
+  "http://ANBALAGANES6.github.io/app251/precache-manifest.f48613f813945464750dbb414362e66e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/app251/index.html", {
+workbox.routing.registerNavigationRoute("http://ANBALAGANES6.github.io/app251/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
