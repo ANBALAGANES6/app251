b24b1e92929c0ae3477a0df90f86e02d043f030c
anbut02 <49370690+anbut02@users.noreply.github.com>
2019-04-08 18:55:48 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch,Redirect } from 'react-router-dom';
+import Home from './Home';
+import NotFounds from './components/NotFound'
+import Edit from './components/Edit'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  render(){
+    return(
+      <BrowserRouter>
+      
+      
+        <Route path="/opportunity/:opportunity_id" exact name="Home" component={Home} />
+        <Route path="/Edit/:edit_id" exact name="Edit" component={Edit} />
+        <Route path="/notfound" exact name="notfound" component={NotFounds} />
+     
+    </BrowserRouter>
+    )
   }
 }
-
 export default App;
